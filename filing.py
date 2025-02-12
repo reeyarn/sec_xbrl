1e607885d98948c0927bdb971fcd3825cfd1363a
reeyarn <reeyarn@gmail.com>
2025-02-12 11:34:33 +0100

    class Statements
    added keywords to class Statements

--- filing.py
+++ filing.py
@@ -14,13 +14,24 @@ FILING_SUMMARY_FILE = 'FilingSummary.xml'
 
 import re
 
+"""
+1001003 - Statement - CONSOLIDATED BALANCE SHEETS
+1002004 - Statement - CONSOLIDATED BALANCE SHEETS (Parenthetical)
+1003005 - Statement - CONSOLIDATED INCOME STATEMENTS
+1004006 - Statement - CONSOLIDATED STATEMENTS OF COMPREHENSIVE INCOME
+1005007 - Statement - CONSOLIDATED SHAREHOLDERS' EQUITY STATEMENTS
+1006008 - Statement - CONSOLIDATED CASH FLOWS STATEMENTS
+"""
 
 class Statements:
     # used in parsing financial data; these are the statements we'll be parsing
     # To resolve "could not find anything for ShortName..." error, likely need
     # to add the appropriate ShortName from the FilingSummary.xml here.
     # TODO: perhaps add guessing/best match functionality to limit this list
-    income_statements = ['consolidated statements of income',
+    income_statements = ['income statement',
+                    'statement income statement',
+                    "statement consolidated income statement",
+                    'consolidated statements of income',
                     'consolidated statements of operations',
                     'consolidated statement of earnings',
                     'condensed consolidated statements of income (unaudited)',
@@ -31,9 +42,16 @@ class Statements:
                     'condensed consolidated statement of earnings',
                     'condensed statements of income',
                     'condensed statements of operations',
-                    'condensed statements of operations and comprehensive loss'
+                    'condensed statements of operations and comprehensive loss',
+                    
                     ]
-    balance_sheets = ['consolidated balance sheets',
+    balance_sheets = [
+                    'balance sheet',
+                    'balance sheets',
+                    'statement balance sheet'
+                    'statement balance sheets'
+                    "statement consolidated balance sheet",
+                    'consolidated balance sheets',
                     'consolidated statement of financial position',
                     'condensed consolidated statement of financial position (current period unaudited)',
                     'condensed consolidated statement of financial position (unaudited)',
@@ -41,12 +59,22 @@ class Statements:
                     'condensed consolidated balance sheets (current period unaudited)',
                     'condensed consolidated balance sheets (unaudited)',
                     'condensed consolidated balance sheets',
-                    'condensed balance sheets'
+                    'condensed balance sheets',
                     ]
-    cash_flows = ['consolidated statements of cash flows',
+    cash_flows = [
+                    "statement consolidated cash flows",
+                    "statement consolidated cash flow statements",
+                    "statement consolidated statement of cash flows",
+                    'consolidated statements of cash flows',
                     'condensed consolidated statements of cash flows (unaudited)',
                     'condensed consolidated statements of cash flows',
                     'condensed statements of cash flows'
+                    "cash flow statement",
+                    "cash flows statement",
+                    "cash flows",
+                    "cash flow",
+                    "cashflow",
+                    'statement cash flow'
                     ]
     retained_earnings = ['consolidated retained earnings',
                     'consolidated statement of retained earnings',
@@ -55,6 +83,16 @@ class Statements:
                     'condensed consolidated statement of retained earnings (unaudited)',
                     "statement of changes in common shareholders' equity",
                     "consolidated statement of changes in common shareholders' equity",
+                    "shareholders' equity statements",
+                    "shareholders' equity statement",
+                    "shareholders' equity",
+                    "shareholders equity",
+                    "shareholder equity",
+                    "statement shareholders equity",
+                    "retained earnings",
+                    "retained earnings statement",
+                    "retained earnings statements",
+                    "statement retained earnings"
                     ]
 
     all_statements = income_statements + balance_sheets + cash_flows + retained_earnings
@@ -209,12 +247,13 @@ class Filing:
         financial_data = []
 
         for names in self._get_statement(statement_short_names):
+            #names = self._get_statement(statement_short_names)[0]
             short_name = names[0]
             filename = names[1]
             #print('Getting financial data for {0} (filename: {1})'.format(short_name, filename))
             financial_html_text = self.documents[filename].doc_text.data
 
-            financial_report = get_financial_report(self.company, self.date_filed, financial_html_text)
+            financial_report = get_financial_report(company = self.company, date_filed = self.date_filed, financial_html_text = financial_html_text)
 
             if get_all:
                 financial_data.append(financial_report)
@@ -231,23 +270,68 @@ class Filing:
         statement_short_names in filing_summary_xml
         '''
         statement_names = []
-
+        str_statements = ""; 
+        str_documents = ""; 
+        num_statements = 0
         if FILING_SUMMARY_FILE in self.documents:
             filing_summary_doc = self.documents[FILING_SUMMARY_FILE]
             filing_summary_xml = filing_summary_doc.doc_text.xml
+            xml_string = str(filing_summary_xml)
+            res_longnames = re.findall(r'<longname>(.*?)</longname>', xml_string)
+            if len(res_longnames) == 0:
+                raise ValueError(f'No financial documents in this filing: {self.url}')
+            else:
+                str_documents = "\n".join(res_longnames)
+                for longname in res_longnames:
+                    if re.search(r'statement', longname, re.IGNORECASE):
+                        num_statements += 1
+                        str_statements += f"{longname}\n"
+                #print(f'Number of statements in this filing: {num_statements}')
+                if num_statements == 0:
+                    str_statements = '\n   '.join(res_longnames)
+                    print(f"Documents in this filing: \n{str_statements}")
+                    raise ValueError(f'No financial documents in this filing: {self.url}')
+
 
             for short_name in statement_short_names:
                 filename = self.get_html_file_name(filing_summary_xml, short_name)
                 if filename is not None:
                     statement_names += [(short_name, filename)]
+        else:
+            raise ValueError(f'No financial documents in this filing: {self.url}')            
         # else:
         #     print('No financial documents in this filing')
 
         if len(statement_names) == 0:
-            print('No financial documents could be found. Likely need to \
-            update constants in edgar.filing.Statements.')
+            print(f"Documents in this filing (for debugging): \n{str_documents}")
+            raise ValueError('No financial documents could be found. Likely need to update constants in edgar.filing.Statements. See above printout for the list of documents in this filing.')
             
         return statement_names
+    
+    def print_statement_names_raw(self):
+        if FILING_SUMMARY_FILE in self.documents:
+            filing_summary_doc = self.documents[FILING_SUMMARY_FILE]
+            filing_summary_xml = filing_summary_doc.doc_text.xml
+            # Solution 3: Use explicit parser
+            xml_string = str(filing_summary_xml)
+            res_longnames = re.findall(r'<longname>(.*?)</longname>', xml_string)
+            if len(res_longnames) == 0:
+                raise ValueError(f'No financial documents in this filing: {self.url}')
+            else:
+                num_statements = 0
+                for longname in res_longnames:
+                    if re.search(r'statement', longname):
+                        num_statements += 1
+                #print(f'Number of statements in this filing: {num_statements}')
+                if num_statements == 0:
+                    raise ValueError(f'No financial documents in this filing: {self.url}')
+            with open("/tmp/filing_summary.xml", "w") as f:
+                f.write(xml_string)
+            xml_bytes = xml_string.encode('utf-8')
+            #parser = lxml_etree.XMLParser(encoding='utf-8')
+            xml = lxml_etree.fromstring(xml_bytes)
+            for child in xml.getchildren():
+                print(child.tag)
 
 
 
@@ -275,6 +359,7 @@ class Filing:
                 continue
             # otherwise, get the text and keep procesing
             short_name = short_name.get_text().lower()
+            short_name = re.sub(r'[^a-zA-Z\s]', ' ', short_name)
 
             # we want to make sure it matches, up until the end of the text
             this_ratio = fuzz.ratio(short_name, report_short_name.lower())
@@ -292,12 +377,13 @@ class Filing:
 
 
     def get_income_statements(self):
-        return self._get_financial_data(self.STATEMENTS.income_statements, False)
+        return self._get_financial_data(statement_short_names = self.STATEMENTS.income_statements, get_all = False)
 
     def get_balance_sheets(self):
-        return self._get_financial_data(self.STATEMENTS.balance_sheets, False)
+        return self._get_financial_data(statement_short_names = self.STATEMENTS.balance_sheets, get_all = False)
 
     def get_cash_flows(self):
-        return self._get_financial_data(self.STATEMENTS.cash_flows, False)
+        return self._get_financial_data(statement_short_names = self.STATEMENTS.cash_flows, get_all = False)
+
     def get_retained_earnings(self):
-        return self._get_financial_data(self.STATEMENTS.retained_earnings, False)
+        return self._get_financial_data(statement_short_names = self.STATEMENTS.retained_earnings, get_all = False)
